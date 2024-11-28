90dac55c4eab3ddfbc4f9aec8688d217467f43d1
Vedant Malgundkar <g22.vedant.malgundkar@gnkhalsa.edu.in>
2024-11-28 17:30:27 +0530

    added utilities

--- src/index.js
+++ src/index.js
@@ -1,8 +1,41 @@
 import connectDB from "./db/index.js";
 
-connectDB();
+import { app } from "./app.js"
+
+const PORT = process.env.PORT || 8000 
+
+connectDB()
+.then(()=>{
+    app.listen(PORT,()=>{
+        console.log(`server is running on  http://localhost:${PORT}`);
+    })
+})
+.catch((error)=>{
+    console.log(`MongoDB connection failed : ${error}`);
+});
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import 'dotenv/config';
+// import mongoose from "mongoose";
+// import express from "express";
+// import { DB_NAME } from './constants.js';
+
+// const app = express()
+// const MONGODB_URI = process.env.MONGODB_URI;
 
 // (async ()=>{
 //     try{
